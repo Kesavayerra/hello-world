57f4368a20ef51d6c54d2273646cd906b096fa87
kesav yerra <kesavyerra@yahoo.in>
2023-07-11 12:53:43 -0400

    Add Welcome message 2 time

--- src/main/java/com/careerit/cj/HelloWorld.java
+++ src/main/java/com/careerit/cj/HelloWorld.java
@@ -3,5 +3,6 @@ package com.careerit.cj;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Welcome to Java World");
+        System.out.println("Once again welcome to Java World");
     }
 }
